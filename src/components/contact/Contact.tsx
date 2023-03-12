55705d604be53117c9ab8ffe295d624006263466
Yevhen Bashchak <evgeny221@gmail.com>
2023-03-12 18:53:33 +0100

    update download button

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,15 +7,13 @@ import { GrSend } from "react-icons/gr"
 export const Contact = () => {
     return (
         <ContactBox>
-            <StyledCvButton download="CV.pdf" href='../../image/YEVHEN_BASHCHAK.pdf' >Download CV <FileDownloadIcon /></StyledCvButton>
+            <StyledCvButton download href="https://drive.google.com/u/0/uc?id=1vpX4sa2m5b3DjsG-NTdCKt1qWNd5fqXQ&export=download" >Download CV <FileDownloadIcon /></StyledCvButton>
             <StyledSendMail href="mailto:evgeny221@gmail.com">
                 Send mail<IconStyled> <GrSend color="blue" /></IconStyled>
             </StyledSendMail>
         </ContactBox>
     )
 }
-
-
 
 
 
