306741470784bbf31504f41e0b2f4a2e38bf05d6
BashchakYevhen <95920987+BashchakYevhen@users.noreply.github.com>
2023-03-30 13:55:35 +0200

    Update Contact.tsx

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,7 +7,7 @@ import { GrSend } from "react-icons/gr"
 export const Contact = () => {
     return (
         <ContactBox>
-            <StyledCvButton download href="https://drive.google.com/u/0/uc?id=1vpX4sa2m5b3DjsG-NTdCKt1qWNd5fqXQ&export=download" >Download CV <FileDownloadIcon /></StyledCvButton>
+            <StyledCvButton download href="https://drive.google.com/u/0/uc?id=1SX75hAgZqKH9GL2qJPR8jsw55n_7NOgL&export=download" >Download CV <FileDownloadIcon /></StyledCvButton>
             <StyledSendMail href="mailto:evgeny221@gmail.com">
                 Send mail<IconStyled> <GrSend color="blue" /></IconStyled>
             </StyledSendMail>
